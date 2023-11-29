e6626b15c9f58ff6c660af9d125423556c911f4f
Ajai Cheemmanam <ajaichemmanam@gmail.com>
2023-11-29 05:28:34 -0500

    fix np.float

--- evaluator/ava_evaluation/metrics.py
+++ evaluator/ava_evaluation/metrics.py
@@ -94,7 +94,7 @@ def compute_average_precision(precision, recall):
 
     if not isinstance(precision, np.ndarray) or not isinstance(recall, np.ndarray):
         raise ValueError("precision and recall must be numpy array")
-    if precision.dtype != np.float or recall.dtype != np.float:
+    if precision.dtype != np.float32 or recall.dtype != np.float32:
         raise ValueError("input must be float numpy array.")
     if len(precision) != len(recall):
         raise ValueError("precision and recall must be of the same size.")
